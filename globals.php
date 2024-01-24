801f54289f2337a73f457e58a75cbbf768439cae
joao-freitas-dev <joao.freitasraf@gmail.com>
2024-01-24 17:20:19 -0300

    changes folder/ config frontend

--- globals.php
+++ globals.php
@@ -2,4 +2,4 @@
     // armazenamento de dados temporários 
     session_start();
     // construção da url 
-    $BASE_URL ="http://" . $_SERVER["SERVER_NAME"] . dirname($_SERVER["REQUEST_URL"]. "?");
+    $BASE_URL ="http://" . $_SERVER["SERVER_NAME"] . dirname($_SERVER["REQUEST_URI"]. "?") . "/";
