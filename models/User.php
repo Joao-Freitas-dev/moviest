4e3291934aad1b1190553b6d5e8114b6e2f5b96f
joao-freitas-dev <joao.freitasraf@gmail.com>
2024-01-25 11:22:11 -0300

    create hash function generate

--- models/User.php
+++ models/User.php
@@ -1,31 +1,40 @@
 <?php
 
-    //pegando nossos campos de user
-  class User {
+//pegando nossos campos de user
+class User
+{
 
-    public $id;
-    public $name;
-    public $lastname;
-    public $email;
-    public $password;
-    public $image;
-    public $bio;
-    public $token;
+  public $id;
+  public $name;
+  public $lastname;
+  public $email;
+  public $password;
+  public $image;
+  public $bio;
+  public $token;
 
+  public function generateToken()
+  {
+    return bin2hex(random_bytes(50));
   }
-
-  //contrato para utilização dos métodos corretos para ser implementados na classe em UserDao
-  interface UserDAOInterface {
-
-    public function buildUser($data);
-    public function create(User $user, $authUser = false);
-    public function update(User $user);
-    public function findByToken($token);
-    public function verifyToken($protected = true);
-    public function setTokenToSession($token, $redirect = true);
-    public function authenticateUser($email, $password);
-    public function findByEmail($email);
-    public function findById($id);
-    public function changePassword(User $user);
-
+  public function generatePassword($password)
+  {
+    return password_hash($password, PASSWORD_DEFAULT);
   }
+}
+
+//contrato para utilização dos métodos corretos para ser implementados na classe em UserDao
+interface UserDAOInterface
+{
+
+  public function buildUser($data);
+  public function create(User $user, $authUser = false);
+  public function update(User $user);
+  public function findByToken($token);
+  public function verifyToken($protected = true);
+  public function setTokenToSession($token, $redirect = true);
+  public function authenticateUser($email, $password);
+  public function findByEmail($email);
+  public function findById($id);
+  public function changePassword(User $user);
+}
