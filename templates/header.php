a88a0569787f013605e885d4f1ecd4345bf78c99
joao-freitas-dev <joao.freitasraf@gmail.com>
2024-01-25 17:18:06 -0300

    Update template header frontend

--- templates/header.php
+++ templates/header.php
@@ -1,40 +1,44 @@
 <?php
-    //incluindo/verificando configurações
-    require_once("globals.php");
-    require_once("db.php");
-    require_once('models/Message.php');
+//incluindo/verificando configurações
+require_once("globals.php");
+require_once("db.php");
+require_once('models/Message.php');
+require_once("dao/UserDAO.php");
 
-    $message = New Message($BASE_URL);
+$message = new Message($BASE_URL);
 
-    $flassMessage = $message->getMessage();
-  
-    if(!empty($flassMessage["msg"])) {
-      // Limpar a mensagem
-      $message->clearMessage();
-      
-    }
+$flassMessage = $message->getMessage();
+
+if (!empty($flassMessage["msg"])) {
+    // Limpar a mensagem
+    $message->clearMessage();
+}
+$userDao = new UserDAO($conn, $BASE_URL);
+$userData = $userDao->verifyToken(false);
+
 ?>
 <!doctype html>
 <html lang="pt">
-  <head>
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="<?= $BASE_URL ?>css/style.css">
     <title>moviest</title>
-  </head>
-  <body>
+</head>
+
+<body>
     <header>
         <nav id="main-navbar" class="navbar navbar-expand-lg">
             <a href="<?= $BASE_URL ?>" class="navbar-brand">
                 <span id="moviestar-title">Moviest</span>
             </a>
 
-            <button class="navbar-toggler" type="button" data-toggle="collapse"
-            data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle
             navigation">
                 <i class="fas fa-bars"></i>
             </button>
@@ -47,16 +51,26 @@
             </form>
             <div class="collapse navbar-collapse" id="navbar">
                 <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a href="<?= $BASE_URL ?>auth.php" class="nav-link"> Entrar/cadastrar</a>
-                    </li>
+                    <?php if ($userData) : ?>
+                        <li class="nav-item">
+                            <a href="<?= $BASE_URL ?>editprofile.php" class="nav-link bold">
+                                <?= $userData->name ?>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="<?= $BASE_URL ?>logout.php" class="nav-link">Sair</a>
+                        </li>
+                    <?php else : ?>
+                        <li class="nav-item">
+                            <a href="<?= $BASE_URL ?>auth.php" class="nav-link">Entrar / Cadastrar</a>
+                        </li>
+                    <?php endif; ?>
                 </ul>
             </div>
         </nav>
     </header>
-    <?php if(!empty($flassMessage["msg"])): ?>
-    <div class="msg-container">
-      <p class="msg <?= $flassMessage["type"] ?>"><?= $flassMessage["msg"] ?></p>
-    </div>
-  <?php endif; ?>
-    
+    <?php if (!empty($flassMessage["msg"])) : ?>
+        <div class="msg-container">
+            <p class="msg <?= $flassMessage["type"] ?>"><?= $flassMessage["msg"] ?></p>
+        </div>
+    <?php endif; ?>
